184a85e5b281c0412f09956348322382cae9cdb8
georgKeinanen <georg.keinanen@gmail.com>
2026-08-18 19:38:23 +0300

    Muokattu tehtävä 3.1

--- Ohjelmointi tehtävä 3.1.py
+++ Ohjelmointi tehtävä 3.1.py
@@ -1,4 +1,4 @@
-kuha_long=float(input("Montako senttimetriä saamasi kuha on: "))
+kuha_long=float(input("Anna kuhan pituus: "))
 if kuha_long<37:
     print("Saamasi kuha on ala mittainen. Laske se takasin veteen heti nyt VÄLITTOMÄSTI!!!!! >:O")
     print(f"Saamasi kuhan pyyntimitasta puuttuu {37-kuha_long}")
