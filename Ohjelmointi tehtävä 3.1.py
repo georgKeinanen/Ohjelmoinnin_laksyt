938e260811f058274623ed14ebbab5249f6b7cba
georgKeinanen <georg.keinanen@gmail.com>
2026-08-18 14:59:22 +0300

    Tehty tehtävä 3.1

---
+++ Ohjelmointi tehtävä 3.1.py
@@ -0,0 +1,7 @@
+kuha_long=float(input("Montako senttimetriä saamasi kuha on: "))
+if kuha_long<37:
+    print("Saamasi kuha on ala mittainen. Laske se takasin veteen heti nyt VÄLITTOMÄSTI!!!!! >:O")
+    print(f"Saamasi kuhan pyyntimitasta puuttuu {37-kuha_long}")
+
+else:
+    print("kalan pyyntimitta on riitävä, voit syödä sen :D")
