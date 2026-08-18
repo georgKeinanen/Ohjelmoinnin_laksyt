5f6658f0afa01ebd923aef6fa9b099a236b2bc05
georgKeinanen <georg.keinanen@gmail.com>
2026-08-18 13:39:22 +0300

    Initial commit

---
+++ Ohjelmointi tehtävä 2.3.py
@@ -0,0 +1,7 @@
+#Ohjemointi tehtävä 3: Suorakulmion pinta-lala
+
+kanta=float(input("Anna suorakulmion kannan pituus: "))
+korko=float(input("Anna suorakulmion korkeus: "))
+
+pinta_ala=(kanta*korko)
+print(f"Suorakulmion pinta-ala on {pinta_ala:.2f}")
