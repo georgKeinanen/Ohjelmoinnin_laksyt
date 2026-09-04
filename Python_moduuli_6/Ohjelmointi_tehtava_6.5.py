57422e052f1a529e06598b8cc89d8131f90cf7d5
georgKeinanen <georg.keinanen@gmail.com>
2026-09-04 09:06:00 +0300

    Korjattu tehtävä 6.5

--- Python_moduuli_6/Ohjelmointi_tehtava_6.5.py
+++ Python_moduuli_6/Ohjelmointi_tehtava_6.5.py
@@ -1,14 +1,16 @@
 # Ohjelmointi_tehtava 6.4: listan alkioiden summa
 
 def odds_remove(lista):
-    for luku in lista[0:-1]:
+    parilliset = []
+    print(f"Alkuperäinen: {lista}")
+    for luku in lista[0:len(lista)]:
+
         # jostain syystä for silmukka ei iteroi listaa kunolla
         # jos iteroida kopiota siitä joka on lista[0:-1]
-        if luku % 2 != 0:
-            # print(i)
-            lista.remove(luku)
+        if luku % 2 == 0:
+            parilliset.append(luku)
 
-    return lista
+    return f"Parittomat luvut postettu: {parilliset}"
 
 
 lista = [2, 4, 3, 5, 8, 13, 23, 33, 3, 24]
