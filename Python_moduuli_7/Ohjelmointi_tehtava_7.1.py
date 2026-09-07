1d2df04e693f1bf564a5c4848d54f69facd69b5b
georgKeinanen <georg.keinanen@gmail.com>
2026-09-07 08:58:09 +0300

    Korjattu tehtävä 6.5

--- Python_moduuli_7/Ohjelmointi_tehtava_7.1.py
+++ Python_moduuli_7/Ohjelmointi_tehtava_7.1.py
@@ -3,16 +3,16 @@
 vuodenajat=("talvi","kevät","kesä","syksy")
 kuukaus_nro=int(input("Anna kuukauden numero: "))
 
-if kuukaus_nro in (1,2,3):
+if kuukaus_nro in (12,1,2):
   print(vuodenajat[0])
 
-elif kuukaus_nro in (4,5,6):
+elif kuukaus_nro in (3,4,5):
   print(vuodenajat[1])
 
-elif kuukaus_nro in (7,8,9):
+elif kuukaus_nro in (6,7,8):
   print(vuodenajat[2])
 
-elif kuukaus_nro in (10,11,12):
+elif kuukaus_nro in (9,10,11):
   print(vuodenajat[3])
 
 else:
